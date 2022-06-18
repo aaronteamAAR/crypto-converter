12dd21f0cc51371a7d15adc67bc49cb3c46f084e
Aaron Chris <rooneychris83@gmail.com>
2022-06-18 14:21:40 +0100

    box shadow

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Currency from "./components/CurrencyConverter"
 const App = () => {
   return (
     <div className="app">
+      <h1>Crypto Dashboard</h1>
+      <div className="app-wrapper">
       <Currency />
       <NewsFeed />
+      </div>
+    
 
     </div>
   );
