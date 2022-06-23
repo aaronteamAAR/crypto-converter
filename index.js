8efc85fca377a61b1e0d2bfae68879a9c478cfcf
Aaron Chris <rooneychris83@gmail.com>
2022-06-23 02:12:25 +0100

    Added backend file

---
+++ index.js
@@ -0,0 +1,64 @@
+const PORT = 8080
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.json('Hey from the backend') 
+}) 
+
+app.get('/converter', (req,res) => {
+    const options = {
+        method: "GET",
+        url: "https://alpha-vantage.p.rapidapi.com/query",
+        params: {
+          from_currency: 'BTC',
+          function: "CURRENCY_EXCHANGE_RATE",
+          to_currency: 'USD',
+        },
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+          "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
+        },
+      };
+  
+      axios
+        .request(options)
+        .then((response) => {
+            res.json(response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+}) 
+
+app.get('/news', (req,res) => {
+    const options = {
+        method: "GET",
+        url: "https://cryptocurrency-news-live.p.rapidapi.com/bitcoin-news",
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+          "X-RapidAPI-Host": "cryptocurrency-news-live.p.rapidapi.com",
+        },
+      };
+  
+      axios
+        .request(options)
+        .then(function (response) {
+          res.json(response.data);
+         
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+})
+
+
+app.listen(8080, () => console.log(`Backend Activated on port ${PORT}`))
--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import ExchangeRate from "./components/ExchangeRate";
 import NewsFeed from "./components/NewsFeed";
-import Currency from "./components/CurrencyConverter"
+import Currency from "./components/CurrencyConverter";
 
 const App = () => {
   return (
     <div className="app">
       <h1>Crypto Dashboard</h1>
       <div className="app-wrapper">
-      <Currency />
-      <NewsFeed />
+        <Currency />
+        <NewsFeed />
       </div>
-    
-<footer>Convert your  <mark>cryptocurrency </mark>  to another with live exchange rates available for you today, Read the headlines to trending crypto news and<br /> discover more on them by click on the headlines🚀
-<section>
-  Made with ❤️ by zilam.inc
-  
-  </section>
-  </footer>
+
+      <footer>
+        Convert your <mark>cryptocurrency </mark> to another with live exchange
+        rates available for you today, Read the headlines to trending crypto
+        news and
+        <br /> discover more on them by click on the headlines🚀
+        <section>Made with ❤️ by zilam.inc</section>
+      </footer>
     </div>
   );
-}
-
-export default App
+};
+export default App;
